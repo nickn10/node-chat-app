59c01c9228c1a61668923095d01c309cb1094f0b
Nick Nunez <nickn10@comcast.net>
2017-12-11 11:46:33 -0600

    Add form for and show messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,16 @@
 var socket = io();
 
+const form = document.querySelector('#message-form')
+const send = document.querySelector('#message-send')
+const messages = document.querySelector('#messages');
+
+
 socket.on('connect', function() {
 	console.log('Connected to server.');
+});
 
-// 	socket.emit('createEmail', {
-// 		to: 'jen@example.com',
-// 		text: 'Hey.  This is Andrew'
-// 	});
-
-// 	socket.emit('createMessage', {
-// 		to: 'neil@example.com',
-// 		text: 'What happened'
-// 	})
+socket.on('disconnect', function() {
+	console.log('Disconnected from server.');
 });
 
 socket.on('newEmail', function(email) {
@@ -20,8 +19,24 @@ socket.on('newEmail', function(email) {
 
 socket.on('newMessage', function(message) {
 	console.log('New message', message);
+	let entry = document.createElement('li');
+	entry.appendChild(document.createTextNode(`${message.from}: ${message.text}`))
+	messages.appendChild(entry);
 });
 
-socket.on('disconnect', function() {
-	console.log('Disconnected from server.');
-});
+// socket.emit('createMessage', {from: 'Andy', text: 'New message'}, (data) => {
+// 	console.log('Got it', data);
+// });
+
+
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: form.message.value
+	}, function() {
+
+	})
+})
